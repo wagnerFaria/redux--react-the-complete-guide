23455000555b89f562b5ae6e00a7f21c931323e3
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-04-17 22:23:50 -0400

    Aula 254 - Multiple reducers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import { createStore, combineReducers } from 'redux';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+});
+
+const store = createStore(rootReducer);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,7 +1,6 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 }
 const reducer = (state = initialState, action) => {
 
@@ -14,14 +13,6 @@ const reducer = (state = initialState, action) => {
             return { ...state, counter: state.counter + action.val }
         case actionTypes.SUBTRACT:
             return { ...state, counter: state.counter - action.val }
-        case actionTypes.STORE_RESULT:
-            return { ...state, results: state.results.concat({ id: new Date(), value: state.counter }) }
-        case actionTypes.DELETE_RESULT:
-            // const id = action.id;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return { ...state, results: updatedArray }
         default:
             return state;
     }
